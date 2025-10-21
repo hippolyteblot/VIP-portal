790c830f1420b15b1e212411f1200c59dae4e03a
Ethaniel Billon <ebillon@student.42lyon.fr>
2025-10-21 11:05:22 +0200

    put vip-application beans into models/ folder

--- vip-application/src/main/java/fr/insalyon/creatis/vip/application/server/business/SimulationBusiness.java
+++ vip-application/src/main/java/fr/insalyon/creatis/vip/application/server/business/SimulationBusiness.java
@@ -1,20 +1,5 @@
 package fr.insalyon.creatis.vip.application.server.business;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Lookup;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
