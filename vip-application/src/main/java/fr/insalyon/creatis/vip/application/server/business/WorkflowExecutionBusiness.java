4877a263c6b6989e1a0847c7cd7c4af880110e39
Hippolyte Blot <hippolytebl@gmail.com>
2026-06-23 15:59:04 +0200

    fix for PR

--- vip-application/src/main/java/fr/insalyon/creatis/vip/application/server/business/WorkflowExecutionBusiness.java
+++ vip-application/src/main/java/fr/insalyon/creatis/vip/application/server/business/WorkflowExecutionBusiness.java
@@ -32,13 +32,11 @@ public class WorkflowExecutionBusiness extends CommonBusiness {
 
     private Server server;
     private WorkflowEngineInstantiator engine;
-    private EngineBusiness engineBusiness;
 
     @Autowired
-    public WorkflowExecutionBusiness(Server server, WorkflowEngineInstantiator engine, EngineBusiness engineBusiness) {
+    public WorkflowExecutionBusiness(Server server, WorkflowEngineInstantiator engine) {
         this.server = server;
         this.engine = engine;
-        this.engineBusiness = engineBusiness;
     }
 
     public String launch(String workflowName, Resource resource, Engine engine, AppVersion appVersion,
@@ -60,13 +58,8 @@ public class WorkflowExecutionBusiness extends CommonBusiness {
         }
     }
 
-    public WorkflowStatus getStatus(String engineName, String simulationID) throws VipException {
-        String endpoint = engineBusiness.get().stream()
-                .filter(e -> e.getName().equals(engineName))
-                .map(Engine::getEndpoint)
-                .findFirst()
-                .orElse(engineName);
-        return engine.getStatus(endpoint, simulationID);
+    public WorkflowStatus getStatus(String engineEndpoint, String simulationID) throws VipException {
+        return engine.getStatus(engineEndpoint, simulationID);
     }
 
     public void kill(String engineEndpoint, String simulationID) throws VipException {
