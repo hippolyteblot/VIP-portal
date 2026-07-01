b783c6c993969e640da895bb9001f53a2b7ae901
Hippolyte Blot <hippolytebl@gmail.com>
2026-07-01 17:14:36 +0200

    adapting to new endpoints for api key

--- frontend/src/api/apikey.api.ts
+++ frontend/src/api/apikey.api.ts
@@ -2,14 +2,14 @@ import { backendClient } from './client'
 
 export const apikeyApi = {
   get(): Promise<string> {
-    return backendClient.get<string>('/internal/apikey').then((r) => r.data)
+    return backendClient.get<string>('/internal/users/me/apikey').then((r) => r.data)
   },
 
   generateNew(): Promise<string> {
-    return backendClient.get<string>('/internal/apikey', { params: { new: true } }).then((r) => r.data)
+    return backendClient.post<string>('/internal/users/me/apikey').then((r) => r.data)
   },
 
   delete(): Promise<void> {
-    return backendClient.delete<void>('/internal/apikey').then((r) => r.data)
+    return backendClient.delete<void>('/internal/users/me/apikey').then((r) => r.data)
   },
 }
